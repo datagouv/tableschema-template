ebeb2deca53831116d5ccd78ba283ee55e8aecdc
Pierlou <pierlou.ramade@data.gouv.fr>
2024-03-11 12:16:43 +0100

    docs: comment code

--- .github/workflows/assert_version.py
+++ .github/workflows/assert_version.py
@@ -8,15 +8,23 @@ version = schema['version']
 
 
 def check(obj, parents=''):
+    """
+    This functions recursively parses all fields in the schema looking for
+    version names that would not be the same as the one mentionned
+    in the 'version' field
+    """
     errors = []
+    # if field is a string, we check for a potential version
     if isinstance(obj, str):
         if homepage in obj:
             tmp = re.search(pattern, obj)
             if tmp and tmp[0] != version:
                 errors += [(parents, tmp[0])]
+    # if field is a list, we check every item
     elif isinstance(obj, list):
         for idx, k in enumerate(obj):
             errors += check(k, parents=parents + f'[{str(idx)}]')
+    # if field is a dict, we check every value
     elif isinstance(obj, dict):
         for k in obj:
             errors += check(
